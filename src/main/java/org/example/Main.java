d3e8c461338171a86185326751ff740ff6fc99dd
FellGast <antonshhavelev@gmail.com>
2024-01-23 22:23:22 +0300

    Calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,64 @@
 package org.example;
 
+import java.util.Scanner;
 /**
- * Hello world!
- *
+ * Calculator with switch function
+ * @author FellGast
+ * @see #main(String[])
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double result;
+        char operation;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ввести первое число");
+        num1 = input.nextDouble();
+
+        System.out.println("Ввести второе число");
+        num2 = input.nextDouble();
+
+        System.out.println("Выбор оператора: +, -, *, или /");
+        operation = input.next().charAt(0);
+
+        switch (operation) {
+
+            // сложение
+            case '+':
+                result = num1 + num2;
+                System.out.printf( "Ответ: " + "%.4f", result );
+                break;
+
+            // вычитание
+            case '-':
+                result = (num1 - num2);
+                System.out.printf("Ответ: " + "%.4f", result);
+                break;
+
+            // умножение
+            case '*':
+                result = num1 * num2;
+                System.out.printf("Ответ: " + "%.4f", result);
+                break;
+
+            // деление
+            case '/':
+                result = num1 / num2;
+                System.out.printf("Ответ: " + "%.4f", result);
+                break;
+
+            default:
+                System.out.println("Нет такого оператора!");
+                break;
+        }
+
+
     }
 }
